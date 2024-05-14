8a749ef33c4112117d307daf94e8ce8b39ba34bf
01455 <95077343+01455@users.noreply.github.com>
2024-05-14 07:38:12 +0300

    add play round function

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,3 +1,6 @@
+let humanScore = 0
+let computerScore = 0
+
 function getComputerChoice() {
     let randomNum = Math.floor(Math.random() * 3 + 1)
     if (randomNum === 1) {
@@ -18,3 +21,35 @@ function getHumanChoice() {
     }   
     return choiceFiltered
 }
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log(`It's a draw! both chose ${humanChoice}`)
+        return
+    }
+    if (humanChoice === 'paper' && computerChoice === 'rock') {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`)
+        humanScore += 1
+        return
+    }
+    if (humanChoice === 'rock' && computerChoice === 'scissors') {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`)
+        humanScore += 1
+        return
+    }
+    if (humanChoice === 'scissors' && computerChoice === 'paper') {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`)
+        humanScore += 1
+        return
+    }
+    else {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
+        computerScore += 1 
+        return
+    }
+}
+
+let humanSelection = getHumanChoice()
+let computerSelection = getComputerChoice()
+
+playRound(humanSelection, computerSelection)
