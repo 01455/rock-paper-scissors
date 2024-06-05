f0bcdece8efae330fa3352559ff3d3fec1032adb
01455 <95077343+01455@users.noreply.github.com>
2024-06-05 05:47:37 +0300

    add ui, code refactor

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -9,67 +9,121 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    let choice = prompt('rock / paper / scissors?', '')
-    let choiceFiltered = choice.toLowerCase().trim()
-    while (choiceFiltered !== 'rock' && choiceFiltered !== 'paper' && choiceFiltered !== 'scissors') {
-        choice = prompt('rock / paper / scissors?', '')
-        choiceFiltered = choice.toLowerCase().trim()
-    }   
-    return choiceFiltered
+function checkScore(human, computer) {
+    if (human == 5) {
+        roundResultTrackerText.textContent = `You won! got ${human} points against computers ${computer}`
+        rockBtn.toggleAttribute('disabled')
+        paperBtn.toggleAttribute('disabled')
+        scissorsBtn.toggleAttribute('disabled')
+        let resetBtn = document.createElement('button')
+        resetBtn.textContent = 'Reset Game'
+        resetBtn.classList.add('btn')
+        buttons.appendChild(resetBtn)
+        resetBtn.addEventListener('click', (e) => {
+            resetGame(e)
+        })
+        return
+    }
+    if (computer == 5) {
+        roundResultTrackerText.textContent = `You lost! got ${human} points against computers ${computer}`
+        rockBtn.toggleAttribute('disabled')
+        paperBtn.toggleAttribute('disabled')
+        scissorsBtn.toggleAttribute('disabled')
+        resetBtn = document.createElement('button')
+        resetBtn.textContent = 'Reset Game'
+        resetBtn.classList.add('btn')
+        buttons.appendChild(resetBtn)
+        resetBtn.addEventListener('click', (e) => {
+            resetGame(e)
+        })
+        return
+    }
 }
 
+let humanChoice = ''
+let humanScore = 0
+let computerScore = 0
+let roundsPlayed = 0
+const rockBtn = document.querySelector('#rockBtn')
+const paperBtn = document.querySelector('#paperBtn')
+const scissorsBtn = document.querySelector('#scissorsBtn')
+const statusDiv = document.querySelector('#gameStatusDiv')
+const buttons = document.querySelector('.btns')
+let humanScoreTrackerText = document.querySelector('#humanScore')
+let computerScoreTrackerText = document.querySelector('#computerScore')
+let roundsPlayedTrackerText = document.querySelector('#roundsPlayed')
+let roundResultTrackerText = document.querySelector('#roundResult')
 
-function playGame(rounds) {
-    let humanScore = 0
-    let computerScore = 0
-    let roundsPlayed = 0
+rockBtn.addEventListener('click', () => {
+    humanChoice = 'rock'
+    playRound(humanChoice, getComputerChoice())
+})
+paperBtn.addEventListener('click', () => {
+    humanChoice = 'paper'
+    playRound(humanChoice, getComputerChoice())
+})
+scissorsBtn.addEventListener('click', () => {
+    humanChoice = 'scissors'
+    playRound(humanChoice, getComputerChoice())
+})
 
-    function playRound(humanChoice, computerChoice) {
-        if (humanChoice === computerChoice) {
-            console.log(`It's a draw! both chose ${humanChoice}`)
-            return
-        }
-        if (humanChoice === 'paper' && computerChoice === 'rock') {
-            console.log(`You win! ${humanChoice} beats ${computerChoice}`)
-            humanScore++
-            return
-        }
-        if (humanChoice === 'rock' && computerChoice === 'scissors') {
-            console.log(`You win! ${humanChoice} beats ${computerChoice}`)
-            humanScore++
-            return
-        }
-        if (humanChoice === 'scissors' && computerChoice === 'paper') {
-            console.log(`You win! ${humanChoice} beats ${computerChoice}`)
-            humanScore++
-            return
-        }
-        else {
-            console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
-            computerScore++
-            return
-        }
-    }
 
-    while (roundsPlayed < rounds) {
-        playRound(getHumanChoice(), getComputerChoice());
-        roundsPlayed++;
-    }
-
-    if (humanScore === computerScore) {
-        console.log(`Draw! both have ${humanScore} point(s)`)
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        roundResultTrackerText.textContent = (` It's a draw! both chose ${humanChoice}`)
+        roundsPlayed++
+        roundsPlayedTrackerText.textContent = 'Rounds Played: ' + roundsPlayed
         return
     }
-
-    if (humanScore > computerScore) {
-        console.log(`You won! you got ${humanScore} points vs computers ${computerScore} points`)
-        return
-    } else {
-        console.log(`You lost! you got ${humanScore} points vs computers ${computerScore} points`)
+    if (humanChoice === 'paper' && computerChoice === 'rock') {
+        roundResultTrackerText.textContent = (` You win! Your pick (${humanChoice}) beats computer pick (${computerChoice})`)
+        humanScore++
+        humanScoreTrackerText.textContent = 'Human Score: ' + humanScore
+        roundsPlayed++
+        roundsPlayedTrackerText.textContent = 'Rounds Played: ' + roundsPlayed
+        checkScore(humanScore, computerScore)
         return
     }
-
+    if (humanChoice === 'rock' && computerChoice === 'scissors') {
+        roundResultTrackerText.textContent = (` You win! Your pick (${humanChoice}) beats computer pick (${computerChoice})`)
+        humanScore++
+        humanScoreTrackerText.textContent = 'Human Score: ' + humanScore
+        roundsPlayed++
+        roundsPlayedTrackerText.textContent = 'Rounds Played: ' + roundsPlayed
+        checkScore(humanScore, computerScore)
+        return
+    }
+    if (humanChoice === 'scissors' && computerChoice === 'paper') {
+        roundResultTrackerText.textContent = (` You win! Your pick (${humanChoice}) beats computer pick (${computerChoice})`)
+        humanScore++
+        humanScoreTrackerText.textContent = 'Human Score: ' + humanScore
+        roundsPlayed++
+        roundsPlayedTrackerText.textContent = 'Rounds Played: ' + roundsPlayed
+        checkScore(humanScore, computerScore)
+        return
+    }
+    else {
+        roundResultTrackerText.textContent = (` You lose! Your pick (${humanChoice}) loses to computer pick (${computerChoice})`)
+        computerScore++
+        computerScoreTrackerText.textContent = 'Computer Score: ' + computerScore
+        roundsPlayed++
+        roundsPlayedTrackerText.textContent = 'Rounds Played: ' + roundsPlayed
+        checkScore(humanScore, computerScore)
+        return
+    }
 }
 
-playGame(5)
+function resetGame(e) {
+    humanScore = 0
+    computerScore = 0
+    roundsPlayed = 0
+    humanScoreTrackerText.textContent = 'Human Score: '
+    computerScoreTrackerText.textContent = 'Computer Score: '
+    roundsPlayedTrackerText.textContent = 'Rounds Played: '
+    roundResultTrackerText.textContent = 'Round Result:'
+    rockBtn.toggleAttribute('disabled')
+    paperBtn.toggleAttribute('disabled')
+    scissorsBtn.toggleAttribute('disabled')
+    buttons.removeChild(e.target)
+    return
+}
